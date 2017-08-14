1c0c2fdc08e9c92e2bab14f715ed3699aba9abe8
Gerard <gerard.purra@gmail.com>
2017-08-14 17:47:34 +0200

    Deleted button to go to main. Use back button instead.

--- src/screens/Sub.js
+++ src/screens/Sub.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text } from 'react-native'
 import { connect } from 'react-redux'
 import { NavigationActions } from 'react-navigation'
 
 const Sub = ({ goToMain }) =>
   <View>
     <Text>Sub content</Text>
-    <Button title="Go to main" onPress={goToMain} />
   </View>
 
 Sub.navigationOptions = {
